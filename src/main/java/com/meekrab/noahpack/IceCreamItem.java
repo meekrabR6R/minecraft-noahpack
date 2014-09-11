49744e8616a5cdd152af2f9d08469675a7e7cd52
Nick Miano <nmiano84@gmail.com>
2014-09-11 16:56:13 -0400

    basic constructor config

--- src/main/java/com/meekrab/noahpack/IceCreamItem.java
+++ src/main/java/com/meekrab/noahpack/IceCreamItem.java
@@ -1,9 +1,16 @@
 package com.meekrab.noahpack;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 /**
  * Created by nmiano on 9/11/14.
  */
 public class IceCreamItem extends Item {
+
+    public IceCreamItem() {
+        setMaxStackSize(64);
+        setCreativeTab(CreativeTabs.tabMisc);
+        setUnlocalizedName("IceCreamItem");
+    }
 }
