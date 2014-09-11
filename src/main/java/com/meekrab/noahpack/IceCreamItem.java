8f50b52efa216013adc1e842ecb3af91c41f747c
Nick Miano <nmiano84@gmail.com>
2014-09-11 16:52:15 -0400

    create icecream item

---
+++ src/main/java/com/meekrab/noahpack/IceCreamItem.java
@@ -0,0 +1,9 @@
+package com.meekrab.noahpack;
+
+import net.minecraft.item.Item;
+
+/**
+ * Created by nmiano on 9/11/14.
+ */
+public class IceCreamItem extends Item {
+}
--- src/main/java/com/meekrab/noahpack/NoahPack.java
+++ src/main/java/com/meekrab/noahpack/NoahPack.java
@@ -2,7 +2,10 @@ package com.meekrab.noahpack;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 
 /**
  * Created by nmiano on 9/11/14.
@@ -12,10 +15,21 @@ public class NoahPack {
     public static final String MODID = "noahpack";
     public static final String VERSION = "1.0";
 
+    //Items
+    public static Item iceCream;
+
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         // some example code
         System.out.println("DIRT BLOCK >> "+ Blocks.dirt.getUnlocalizedName());
     }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent preInitEvent) {
+        iceCream = new IceCreamItem();
+        // The second parameter is an unique registry identifier (not the displayed name)
+        // Please don't use item1.getUnlocalizedName(), or you will make Lex sad
+        GameRegistry.registerItem(iceCream, "iceCream");
+    }
+
 }
