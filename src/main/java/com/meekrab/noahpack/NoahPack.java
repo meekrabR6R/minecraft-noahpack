7126431d318e5eb4ef65c33b0f11aaa373b151c1
Nick Miano <nmiano84@gmail.com>
2014-09-14 18:37:25 -0400

    crafting rules for ice cream

--- src/main/java/com/meekrab/noahpack/NoahPack.java
+++ src/main/java/com/meekrab/noahpack/NoahPack.java
@@ -5,7 +5,9 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 /**
  * Created by nmiano on 9/11/14.
@@ -21,12 +23,21 @@ public class NoahPack {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         // some example code
-        System.out.println("DIRT BLOCK >> "+ Blocks.dirt.getUnlocalizedName());
+        //System.out.println("DIRT BLOCK >> "+ Blocks.dirt.getUnlocalizedName());
     }
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent preInitEvent) {
         iceCreamItem = new IceCreamItem(1, 4);
+
+        ItemStack snowballStack = new ItemStack(Items.snowball);
+        ItemStack sugarStack    = new ItemStack(Items.sugar);
+        ItemStack milkStack     = new ItemStack(Items.milk_bucket);
+        ItemStack bowlStack     = new ItemStack(Items.bowl);
+
+        GameRegistry.addShapelessRecipe(new ItemStack(iceCreamItem, 16),
+                snowballStack, sugarStack, milkStack, bowlStack);
+
         // The second parameter is an unique registry identifier (not the displayed name)
         // Please don't use item1.getUnlocalizedName(), or you will make Lex sad
         GameRegistry.registerItem(iceCreamItem, "iceCreamItem");
